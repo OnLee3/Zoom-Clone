78f3adbdd0ddfe81c0b2f2be81c4cc4680b74fd1
OnLee3 <dlwotjr2@gmail.com>
2021-09-08 22:38:44 +0900

    add URL catch func

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 const PORT = 4000;
 const handleListen = () => console.log(`Server Listening on ${PORT}.`)
 app.listen(PORT, handleListen);
