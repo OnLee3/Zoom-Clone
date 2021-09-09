701ec943d0c10eadb0e626865783967f0d3040a9
OnLee3 <dlwotjr2@gmail.com>
2021-09-09 22:35:20 +0900

    1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http"
 import WebSocket from "ws"
 import express from "express"
+import { parse } from "path";
 
 const app = express();
 
@@ -20,10 +21,19 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✓")
     socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-    socket.on("message", message => {
-        sockets.forEach((asocket) => asocket.send(message.toString("utf8")));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((asocket) => asocket.send(`${socket.nickname} : ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
 });
 
