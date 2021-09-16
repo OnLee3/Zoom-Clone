08bbb212311f6991069f6565182343fb065069b1
OnLee3 <dlwotjr2@gmail.com>
2021-09-16 21:41:14 +0900

    main page CSS

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+//app.get("/*", (req, res) => res.redirect("/"));
 
 const PORT = 4000;
 const handleListen = () => console.log(`Server Listening on http://localhost:${PORT}.`)
